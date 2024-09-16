52df4cdfedc26cde801ca305758985364a3b4750
Fady Mondy <info@3x1.io>
2024-09-16 15:22:16 +0300

    add filament-plugins integration

--- src/FilamentMenusPlugin.php
+++ src/FilamentMenusPlugin.php
@@ -5,12 +5,15 @@ namespace TomatoPHP\FilamentMenus;
 use Filament\Contracts\Plugin;
 use Filament\Panel;
 use Illuminate\View\View;
+use Nwidart\Modules\Module;
 use TomatoPHP\FilamentMenus\Resources\MenuResource;
 use TomatoPHP\FilamentTranslations\Http\Middleware\LanguageMiddleware;
 use TomatoPHP\FilamentTranslations\Resources\TranslationResource;
 
 class FilamentMenusPlugin implements Plugin
 {
+    private bool $isActive = false;
+
     public function getId(): string
     {
         return 'filament-menus';
@@ -18,10 +21,21 @@ class FilamentMenusPlugin implements Plugin
 
     public function register(Panel $panel): void
     {
-        $panel
-            ->resources([
-                MenuResource::class
-            ]);
+        if(class_exists(Module::class)){
+            if(\Nwidart\Modules\Facades\Module::find('FilamentMenus')->isEnabled()){
+                $this->isActive = true;
+            }
+        }
+        else {
+            $this->isActive = true;
+        }
+
+        if($this->isActive) {
+            $panel
+                ->resources([
+                    MenuResource::class
+                ]);
+        }
     }
 
     public function boot(Panel $panel): void
